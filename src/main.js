5d9df10681b46757d6d2acd4c58bbd0918136290
Jason Springer <jsnsprngr@gmail.com>
2017-07-20 14:39:52 -0400

    Add express

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-window.Event = new Vue()
+window.Event = new Vue();
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
